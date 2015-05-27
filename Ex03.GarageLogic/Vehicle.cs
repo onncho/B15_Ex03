bc75948be24f6c07ceff107bbfbdb6eeebe8993c
onn hachmon <onncho@outlook.com>
2015-05-28 01:19:56 +0300

    no message

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -4,12 +4,24 @@ using System.Text;
 
 namespace Ex03.GarageLogic
 {
-    class Vehicle
+    // need to put here arguments but who gonna give it? 
+    class Vehicle : Wheel(//HERE//)
     {
 
         private String m_vehicleName;
         private String m_licenceNumber;
         private float m_reaminingEng;
-        private 
+        private List<Wheel> m_vehicleWheels;
+
+        public Vehicle(String m_vehicleName, String m_licenceNumber, float m_reaminingEng, List<Wheel> m_vehicleWheels)
+        {
+            this.m_vehicleName = m_vehicleName;
+            this.m_licenceNumber = m_licenceNumber;
+            this.m_reaminingEng = m_reaminingEng;
+            this.m_vehicleWheels = m_vehicleWheels;
+
+        }
+
+
     }
 }
